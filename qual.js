144eb6c91f9210c0cf8ddb6172d8d21e38cc5efe
Dmitry Savchenko <dg.freak@gmail.com>
2017-11-21 16:43:13 +0200

    [master] Initial commit

---
+++ qual.js
@@ -0,0 +1,91 @@
+const escomplex = require('typhonjs-escomplex');
+const fs = require('fs');
+const fileSystem = fs;
+const glob = require('glob');
+const childProcess = require('child_process');
+const request = require('request');
+const qs = require('qs');
+const program = require('commander');
+
+program
+  .version('0.0.0')
+  .option('-p, --path [path]', 'Project path')
+  .option('-d, --dry', 'Dry run, data does not go anywhere')
+  .option('-H, --host [host]', 'API host')
+  .option('-P, --port [port]', 'API port')
+  .option('-T, --token [token]', 'API token')
+  .parse(process.argv);
+
+function submitProbeInfo(probeID, token, probeInfo) {
+  var options = {
+    url: 'http://' + program.host + ':' + program.port + '/api/probe_infos',
+    method: "POST",
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Token token=' + token
+    },
+    body: qs.stringify({
+      probe_id: probeID,
+      value: 0,
+      params: probeInfo,
+      quality_characteristics: [ 'performance' ]
+    }, { arrayFormat: 'brackets' })
+  };
+
+  request.post(options, function(error, response, body) {
+    // Do nothing
+  });
+}
+
+glob(program.path + '/{,!(build|node_modules|debug|test|spec|doc|docs)/**/}*.js', function(err, res) {
+  if (err) throw err;
+
+  var currentAmount = 0;
+  var maintainability = 0;
+  var loc = 0;
+
+  var quality = res.forEach(function(filename) {
+    try {
+      var source = fs.readFileSync(filename, 'utf8');
+      var v = escomplex.analyzeModule(source, { newmi: true });
+
+      currentAmount += 1;
+      maintainability = maintainability + (v['maintainability'] - maintainability)/(currentAmount + 1.0);
+      loc += v['lineEnd']
+    } catch (err) {
+      return null;
+    }
+  });
+
+  process.argv = ['', '', '--reporter=json-summary', 'npm', 'run', 'test'];
+  process.chdir(program.path);
+  const nyc = require('./nyc.js');
+  nyc.onFinished = function() {
+    var coverageLoc = 0;
+
+    try {
+      var reportJSON = JSON.parse(fs.readFileSync(program.path + '/coverage/coverage-summary.json', 'utf8'));
+      coverageLoc = loc * reportJSON['total']['lines']['pct'] / 100.0;
+    } catch (err) {
+      // Do nothing
+    }
+
+    revision = childProcess.execSync('cd ' + program.path + ' && git rev-parse HEAD')
+                 .toString().trim()
+    commitDate = childProcess.execSync('cd ' + program.path + ' && git log -1 --format=%cd').toString().trim()
+
+    const probeInfo = {
+      maintainability: maintainability,
+      loc: loc,
+      coverage: coverageLoc,
+      revision: revision,
+      datetime: new Date(commitDate)
+    };
+
+    if (program.dry) {
+      console.log(probeInfo);
+    } else {
+      submitProbeInfo(2, program.token, probeInfo);
+    }
+  };
+});
